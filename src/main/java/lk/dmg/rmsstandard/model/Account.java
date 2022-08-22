27713b4095c490998822d608dc9723760174ed27
Jasintha Peiris <jasintha.p@aventude.com>
2022-08-22 13:25:56 +0530

    init comment

---
+++ src/main/java/lk/dmg/rmsstandard/model/Account.java
@@ -0,0 +1,71 @@
+/*
+ *     Copyright (c) 1995-2022,  The Data Management Group Ltd   All Rights Reserved.
+ *     *  PROPRIETARY AND COPYRIGHT NOTICE.
+ *
+ *        This software product contains information which is proprietary to
+ *        and considered a trade secret The Data management Group Ltd .
+ *        It is expressly agreed that it shall not be reproduced in whole or part,
+ *        disclosed, divulged or otherwise made available to any third party directly
+ *        or indirectly.  Reproduction of this product for any purpose is prohibited
+ *        without written authorisation from the The Data Management Group Ltd
+ *        All Rights Reserved.
+ *
+ *        E-Mail andyj@datam.co.uk
+ *        URL : www.datam.co.uk
+ *        Created By :Jasintha Peiris
+ */
+package lk.dmg.rmsstandard.model;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Date :2022-05-12 This class process the StandardNew model class model class
+ * 
+ * @author Jasintha Peiris
+ */
+@Setter
+@Getter
+@Entity
+@Table(name = "account")
+public class Account{
+	@Id
+	@Column(name = "account_id")
+	private Integer accountId;	
+	
+	@Column(name = "account_name")
+	public String accountName;
+	
+	@Column(name = "accountType")
+	public String accountType;
+	
+	@Column(name = "id_number")
+	private Integer orderNumber;
+
+	@Column(name = "description")
+	public String description;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "created_date")
+	private Date createdDate;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "updated_date")
+	private Date updatedDate;
+
+	@Column(name = "is_active")
+	private Boolean isActive;
+
+	@Column(name = "is_delete")
+	private Boolean isDelete;
+}
